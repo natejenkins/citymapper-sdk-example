fbc45996063c29a2b5df729bd560eb0f42d4c0af
Nathan Jenkins <nate@citymapper.com>
2022-08-06 02:23:59 +0100

    fix

--- src/components/ThemedRouteList/index.tsx
+++ src/components/ThemedRouteList/index.tsx
@@ -1,5 +1,5 @@
 import { RouteList, CMProvider } from '@citymapper/sdk-react'
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export const ThemedRouteList = (props: any) => {
   return (
     <CMProvider
